e2b7355daa85af3c3001602f0c0e36f689f3484c
patrakhin <patrakhin@mail.ru>
2023-05-11 00:50:59 +0300

    add settings

--- src/main/java/ru/patrakhin/mythymeleaf/controllers/ControllerPage1.java
+++ src/main/java/ru/patrakhin/mythymeleaf/controllers/ControllerPage1.java
@@ -1,2 +1,15 @@
-package ru.patrakhin.mythymeleaf.controllers;public class ControllerPage1 {
+package ru.patrakhin.mythymeleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerPage1 {
+        @GetMapping("/")
+        public String index(Model model) {
+            model.addAttribute("pageTitle", "Default Page Title");
+            model.addAttribute("pageText", "Default page text goes here");
+            return "page1";
+        }
 }
